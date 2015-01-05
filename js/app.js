918befb582bf373a5413bb56163be6db55091ff3
jimCresswell <jim.cresswell@gmail.com>
2015-01-05 12:38:31 +0000

    Add a manual geometry and material and use them to insert a custom mesh into the scene.

--- js/app.js
+++ js/app.js
@@ -84,6 +84,31 @@
     scene.add(light);
     scene.add(camera);
     scene.add(box);
+
+
+    /**
+     * Manual geometry and material example.
+     */
+
+    var manualMaterial = new THREE.MeshBasicMaterial({
+      vertexColors: THREE.VertexColors,
+      side: THREE.Doubleside
+    });
+    var triangleGeometry = new THREE.Geometry();
+    triangleGeometry.vertices.push (new THREE.Vector3(0.0, 1.0, 0.0));
+    triangleGeometry.vertices.push (new THREE.Vector3(-1.0, -1.0, 0.0));
+    triangleGeometry.vertices.push (new THREE.Vector3(1.0, -1.0, 0.0));
+
+    triangleGeometry.faces.push(new THREE.Face3(0, 1, 2));
+
+    triangleGeometry.faces[0].vertexColors[0] = new THREE.Color(0xFF0000);
+    triangleGeometry.faces[0].vertexColors[1] = new THREE.Color(0x00FF00);
+    triangleGeometry.faces[0].vertexColors[2] = new THREE.Color(0xFF0000);
+
+    var manualMesh = new THREE.Mesh(triangleGeometry, manualMaterial);
+    manualMesh.position.x = 25;
+    manualMesh.scale.set(10, 10, 10);
+    scene.add(manualMesh);
   }
 
   // Infinite recursive loop.
